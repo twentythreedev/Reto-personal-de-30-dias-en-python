1b3911dfaa1cae077a90d13c61311e3731d423c0
Ivan Gonzalez <ivan.gon.ro@hotmail.com>
2022-10-28 20:18:51 -0500

    Ejercicio 2

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,12 +1,37 @@
-'''Escribí un programa que solicite al usuario que ingrese su nombre. 
+'''
+001
+Escribí un programa que solicite al usuario que ingrese su nombre. 
 El nombre se debe almacenar en una variable llamada nombre. 
 A continuación se debe mostrar en pantalla el texto “Ahora estás en la matrix, [usuario]”, donde “[usuario]” 
 se reemplazará por el nombre que el usuario haya ingresado.
 '''
 
+#Solución>>>
+'''
 nombre = input("Ingrese su nombre ")
 mensaje = "Ahora estas en la matrix "
 
 matrix = mensaje + nombre
 
 print( matrix)
+
+'''
+
+'''
+002
+Escribí un programa que solicite al usuario ingresar un número con decimales y almacenalo en una variable. 
+A continuación, el programa debe solicitar al usuario que ingrese un número entero y guardarlo en otra variable. 
+En una tercera variable se deberá guardar el resultado de la suma de los dos números ingresados por el usuario. 
+Por último, se debe mostrar en pantalla el texto 
+“El resultado de la suma es [suma]”, donde “[suma]” se reemplazará por el resultado de la operación.
+'''
+#Solución>>>
+'''
+a = (input("primer numero: "))
+b = (input("Segundo numero: "))
+
+s = a + b
+
+print("El resultado de la suma es ", s)
+
+'''
