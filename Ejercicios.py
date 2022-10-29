4d9331ea1f42eeb816d412aafa3f1928eed327ad
Ivan Gonzalez <ivan.gon.ro@hotmail.com>
2022-10-28 19:59:20 -0500

    Seccion 1

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,10 +1,8 @@
-n1 = int(input("cuentame tu edad "))
-n2 = int(input("Ahora tu fecha de nacimiento "))
+#Escribí un programa que solicite al usuario que ingrese su nombre. El nombre se debe almacenar en una variable llamada nombre. A continuación se debe mostrar en pantalla el texto “Ahora estás en la matrix, [usuario]”, donde “[usuario]” se reemplazará por el nombre que el usuario haya ingresado.
 
-num1 = n1
-num2 = n2
+nombre = input("Ingrese su nombre ")
+mensaje = "Ahora estas en la matrix "
 
-print(num1)
-print(num2)
+matrix = mensaje + nombre
 
-
+print( matrix)
