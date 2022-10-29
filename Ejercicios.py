de992cd5c7aeb6b8480128c50fdb5467a80a3219
Ivan Gonzalez <ivan.gon.ro@hotmail.com>
2022-10-28 20:02:19 -0500

    Arreglando el enunciado

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,4 +1,8 @@
-#Escribí un programa que solicite al usuario que ingrese su nombre. El nombre se debe almacenar en una variable llamada nombre. A continuación se debe mostrar en pantalla el texto “Ahora estás en la matrix, [usuario]”, donde “[usuario]” se reemplazará por el nombre que el usuario haya ingresado.
+'''Escribí un programa que solicite al usuario que ingrese su nombre. 
+El nombre se debe almacenar en una variable llamada nombre. 
+A continuación se debe mostrar en pantalla el texto “Ahora estás en la matrix, [usuario]”, donde “[usuario]” 
+se reemplazará por el nombre que el usuario haya ingresado.
+'''
 
 nombre = input("Ingrese su nombre ")
 mensaje = "Ahora estas en la matrix "
